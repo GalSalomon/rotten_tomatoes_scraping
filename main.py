80c48e13fdda502f0dc95cc6db176dd29e739dec
GalSalomon <gal.slmn@gmail.com>
2021-02-18 01:01:17 +0200

    Added pandas to requirements.txt and made some progress in the project. need to fix only two functions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import conf
 import grequests
 import logging
 import requests
+import pandas as pd
 import sys
 from bs4 import BeautifulSoup as soup
 from datetime import datetime
@@ -23,6 +24,7 @@ movies_logger = setup_logger('movies_log', 'movies.log')
 
 
 def get_soup_from_response(response):
+    # this function is DONE!
     """receives  a response from requests/grequests and returns soup"""
     page_html = response.content
     # html parser
@@ -32,6 +34,7 @@ def get_soup_from_response(response):
 
 
 def get_responses_from_urls(urls):
+    # this function is DONE!
     """receives urls and returns responses"""
     try:
         movies_logger.info(f'trying to get responses for next {len(urls)} urls')
@@ -47,12 +50,14 @@ def get_responses_from_urls(urls):
 
 
 def get_soups_from_urls(urls):
+    # this function is DONE!
     """receives urls and returns a list of soups"""
     responses = get_responses_from_urls(urls)
     return [get_soup_from_response(response) for response in responses]
 
 
 def get_soup_from_url(url):
+    # this function is DONE!
     """gets a url and returns it's parsed content in bs4.BeautifulSoup type """
     try:
         movies_logger.info(f'Trying to get response for {url}')
@@ -65,7 +70,8 @@ def get_soup_from_url(url):
     return get_soup_from_response(response)
 
 
-def get_directors_from_soup(movie_soup):
+def get_attributes_from_soup(movie_soup):
+    # todo fix this function to get all the required data from the movie page
     """gets soup of a movie url and returns the director/s from the page"""
     movies_logger.info(f'Looking for directors in the soup')
     credit_items = movie_soup.findAll("div", {"class": "credit_summary_item"})
@@ -83,42 +89,47 @@ def get_directors_from_soup(movie_soup):
 
 
 def get_titles_with_bs4(url):
+    # this function is DONE!
+    """receives the url of the current top 100 movies on netflix
+    and returns a list of 100 soups (including the name of the movie and url to it's
+    page on rotten tomatoes)"""
     page_soup = get_soup_from_url(url)
-    movies_logger.info(f'Fetching the movie titles of iMDB top 250 from {url}')
-    titles = page_soup.findAll("td", {"class": "titleColumn"})
-    movies_logger.info(f'Finished fetching the movie titles of iMDB top 250 from {url}')
+    movies_logger.info(f'Fetching the current top 100 movies from Rotten Tomatoes {url}')
+    titles = page_soup.findAll("div", {"class": "article_movie_title"})
+    movies_logger.info(f'Finished fetching the current top 100 movies from Rotten Tomatoes {url}')
     return titles
 
 
-def add_directors_with_grequests(movies):
+def get_attributes_from_movies_urls(movies):
+    # todo get all attributes for each movie
     """gets a list of dictionaries, each dict is a movie
     and added a new key "directors" with a value of the movie's directors"""
     for x in range(int(len(movies) / conf.BATCH_SIZE)):
-        urls = []
+        # runs from 0 till len(movies) / BATCH_SIZE
         movies_logger.info(f'Starting batch {x + 1}/{len(range(int(len(movies) / conf.BATCH_SIZE)))}')
-        for i in range(x * conf.BATCH_SIZE, x * conf.BATCH_SIZE + conf.BATCH_SIZE):
-            urls.append(movies[i]['url'])
-        soups = get_soups_from_urls(urls)
-        soups_index = 0
-        for i in range(x * conf.BATCH_SIZE, x * conf.BATCH_SIZE + conf.BATCH_SIZE):
-            movies[i]['directors'] = get_directors_from_soup(soups[soups_index])
-            soups_index += 1
-            print(f"{i + 1} - {movies[i]['movie_name']} - {movies[i]['directors']}")
-            movies_logger.info(f"{i + 1} - {movies[i]['movie_name']} - {movies[i]['directors']}")
-    return movies
+        soups = get_soups_from_urls(movies['url'][x*conf.BATCH_SIZE:x*conf.BATCH_SIZE+conf.BATCH_SIZE])
+        # todo: take it from here. I need to find the relevant data in the page and add it to the pandas df
+#         soups_index = 0
+#         for i in range(x * conf.BATCH_SIZE, x * conf.BATCH_SIZE + conf.BATCH_SIZE):
+#             movies[i]['directors'] = get_attributes_from_soup(soups[soups_index])
+#             soups_index += 1
+#             print(f"{i + 1} - {movies[i]['movie_name']} - {movies[i]['directors']}")
+#             movies_logger.info(f"{i + 1} - {movies[i]['movie_name']} - {movies[i]['directors']}")
+    return soups  # movies
 
 
-def get_imdb_top_250_with_grequests(url=conf.IMDB_TOP_250_URL):
+def get_top_movies_on_rotten_tomatoes(url=conf.TOMATO_BEST_MOVIES):
+    # this function is DONE!
     """this function gets no input and prints the top 250 movies and their directors according to imdb
     but this time with grequests"""
     start = datetime.now()
-    movies_logger.info('Starting now! ("grequests" fast version)')
+    movies_logger.info(f'Starting to fetch all movies from {url} now!')
     titles = get_titles_with_bs4(url)
-    movies = []
+    movies = pd.DataFrame(columns=['title', 'url', 'genre', 'length', 'score1', 'score2', 'year', 'poster', 'text'])
     for t in titles:
-        movie_name, url = t.a.contents[0], "https://www.imdb.com" + t.a['href']
-        movies.append({'movie_name': movie_name, 'url': url})
-    movies = add_directors_with_grequests(movies)
+        movie_name, href = t.a.contents[0], t.a['href']
+        movies = movies.append(pd.DataFrame(columns=['title', 'url'], data=[[movie_name, href]]))
+    movies = get_attributes_from_movies_urls(movies)
     print(f'this operation took {datetime.now() - start}')
     movies_logger.info(f'Done!')
     movies_logger.info(f'This operation took {datetime.now() - start}')
@@ -126,7 +137,7 @@ def get_imdb_top_250_with_grequests(url=conf.IMDB_TOP_250_URL):
 
 
 if __name__ == "__main__":
-    get_imdb_top_250_with_grequests()
+    get_top_movies_on_rotten_tomatoes()
 
 """
 # Todo:
